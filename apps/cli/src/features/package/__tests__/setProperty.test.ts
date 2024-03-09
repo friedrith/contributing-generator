df4417fb6bf1d3eda1fd5574cfe1481af53ed1b2
Thibault Friedrich <thibault.friedrich@gmail.com>
2024-03-09 07:05:10 -0500

    test: add unit tests for setProperty

--- apps/cli/src/features/package/__tests__/setProperty.test.ts
+++ apps/cli/src/features/package/__tests__/setProperty.test.ts
@@ -1,84 +1,85 @@
 import setProperty from '../setProperty'
 
+const formattedContent = (content: object) => JSON.stringify(content, null, 2)
+
 describe('setProperty', () => {
   describe('update property', () => {
     it('should update the value of an existing property when it is last property', () => {
-      const content = JSON.stringify(
-        {
-          name: 'test',
-          version: '1.0.0',
-          license: 'GPL-3.0',
-        },
-        null,
-        2
-      )
+      const content = formattedContent({
+        name: 'test',
+        version: '1.0.0',
+        license: 'GPL-3.0',
+      })
 
       const newContent = setProperty(content, 'license', 'MIT')
 
       expect(newContent.content).toBe(
-        JSON.stringify(
-          {
-            name: 'test',
-            version: '1.0.0',
-            license: 'MIT',
-          },
-          null,
-          2
-        )
+        formattedContent({
+          name: 'test',
+          version: '1.0.0',
+          license: 'MIT',
+        })
       )
     })
 
     it('should update the value of an existing property when it is not last property', () => {
-      const content = JSON.stringify(
-        {
-          name: 'test',
-          license: 'GPL-3.0',
-          version: '1.0.0',
-        },
-        null,
-        2
-      )
+      const content = formattedContent({
+        name: 'test',
+        license: 'GPL-3.0',
+        version: '1.0.0',
+      })
 
       const newContent = setProperty(content, 'license', 'MIT')
 
       expect(newContent.content).toBe(
-        JSON.stringify(
-          {
-            name: 'test',
-            license: 'MIT',
-            version: '1.0.0',
-          },
-          null,
-          2
-        )
+        formattedContent({
+          name: 'test',
+          license: 'MIT',
+          version: '1.0.0',
+        })
       )
+    })
+
+    it('should return updated message', () => {
+      const content = formattedContent({
+        name: 'test',
+        license: 'GPL-3.0',
+        version: '1.0.0',
+      })
+
+      const newContent = setProperty(content, 'license', 'MIT')
+
+      expect(newContent.message).toBe('✔ license updated in package.json')
     })
   })
 
   describe('add property', () => {
     it('should add property license', () => {
-      const content = JSON.stringify(
-        {
-          name: 'test',
-          version: '1.0.0',
-        },
-        null,
-        2
-      )
+      const content = formattedContent({
+        name: 'test',
+        version: '1.0.0',
+      })
 
       const newContent = setProperty(content, 'license', 'MIT')
 
       expect(newContent.content).toBe(
-        JSON.stringify(
-          {
-            name: 'test',
-            license: 'MIT',
-            version: '1.0.0',
-          },
-          null,
-          2
-        )
+        formattedContent({
+          name: 'test',
+          license: 'MIT',
+          version: '1.0.0',
+        })
       )
+    })
+
+    it('should return added message', () => {
+      const content = formattedContent({
+        name: 'test',
+        version: '1.0.0',
+      })
+
+      const newContent = setProperty(content, 'license', 'MIT')
+
+      expect(newContent.message).toBe('✔ license added to package.json')
     })
   })
 })
