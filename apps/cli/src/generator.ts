ccc7912b62bc65a0bd450b09032af793008d064c
Thibault Friedrich <thibault.friedrich@gmail.com>
2024-03-09 08:49:43 -0500

    feat: add choice for other target

--- apps/cli/src/generator.ts
+++ apps/cli/src/generator.ts
@@ -1,7 +1,23 @@
+import select from '@inquirer/select'
+
 import generateLicense from './features/licenses/generateLicense'
 
 const generator = async () => {
-  await generateLicense()
+  const choices = ['license', 'readme', 'contributing'].map(choice => ({
+    value: choice,
+    label: choice,
+  }))
+
+  const option = await select({
+    message: 'What do you want to generate?',
+    choices,
+  })
+
+  if (option === 'license') {
+    await generateLicense()
+  } else {
+    console.log('Not implemented yet')
+  }
 }
 
 export default generator
