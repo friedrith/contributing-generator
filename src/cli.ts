1be1f7d4e505c522bb1bb43a52b79bfa400e54f6
Thibault Friedrich <thibault.friedrich@gmail.com>
2024-03-12 08:49:06 -0400

    feat: add argument -v to get the version

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 
 import generator from './generator'
+import packageJson from '../package.json'
 
-generator()
+if (['--version', '-v'].find(arg => process.argv.includes(arg))) {
+  console.log('v' + packageJson.version)
+} else {
+  generator()
+}
