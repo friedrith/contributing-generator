5e286a28d500783bec716366c699354cb8d58b41
Thibault Friedrich <thibault.friedrich@gmail.com>
2024-03-15 10:46:50 -0400

    chore: fix commitlint (#14)

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,7 +1,10 @@
+import type { UserConfig } from '@commitlint/types'
+import { RuleConfigSeverity } from '@commitlint/types'
+
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-max-line-length': 'off',
-    'footer-max-line-length': 'off',
+    'body-max-line-length': [RuleConfigSeverity.Disabled],
+    'footer-max-line-length': [RuleConfigSeverity.Disabled],
   },
-}
+} satisfies UserConfig
