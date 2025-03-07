05c68f4829045ef4a33e1ccd57e7aa6ffc6f06f0
Thibault Friedrich <thibault.friedrich@gmail.com>
2025-03-06 19:14:51 -0500

    ci: remove coverage for release pipeline

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,6 @@ import type { Config } from 'jest'
 
 const config: Config = {
   testMatch: ['<rootDir>/src/**/*.test.{tsx,ts}'],
-  testPathIgnorePatterns: [],
   preset: 'ts-jest',
   moduleNameMapper: {
     '~/(.*)$': '<rootDir>/$1',
