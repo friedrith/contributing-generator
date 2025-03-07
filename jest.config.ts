9f5b74c510800296780454c237a7ad50a3464f8c
Thibault Friedrich <thibault.friedrich@gmail.com>
2025-03-06 19:10:44 -0500

    chore: change ignore

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ import type { Config } from 'jest'
 
 const config: Config = {
   testMatch: ['<rootDir>/src/**/*.test.{tsx,ts}'],
+  testPathIgnorePatterns: [],
   preset: 'ts-jest',
   moduleNameMapper: {
     '~/(.*)$': '<rootDir>/$1',
